a489e1db2923767e9325fb206909aeb08c0e3d01
Steven Mancine <steven.mancine@charter.com>
2024-05-28 21:00:22 -0600

    Add code comments

--- src/Cone.java
+++ src/Cone.java
@@ -6,16 +6,19 @@ public class Cone extends Shape {
     this.height = height;
   }
 
+  // Returns the surface area of a cone
   @Override
   public double surface_area() {
     return Math.PI * radius * (radius + Math.sqrt(Math.pow(height, 2) + Math.pow(radius, 2)));
   }
 
+  // Returns the volume of a cone
   @Override
   public double volume() {
     return Math.PI * Math.pow(radius, 2) * (height/3.0);
   }
 
+  // Prints out the surface area and volume
   @Override
   public String toString() {
     return  "Cone surface area: " + surface_area() + ", cone volume: " + volume() +".";
--- src/Sphere.java
+++ src/Sphere.java
@@ -5,16 +5,19 @@ public class Sphere extends Shape {
     this.radius = radius;
   }
 
+  // Returns the surface area of a sphere
   @Override
   public double surface_area() {
     return 4 * Math.PI * Math.pow(radius, 2);
   }
 
+  // Returns the volume of a sphere
   @Override
   public double volume() {
     return (4.0/3.0) * Math.PI * Math.pow(radius, 3);
   }
   
+  // Prints out the surface area and volume
   @Override
   public String toString() {
     return "Sphere surface area: " + surface_area() + ", sphere volume: " + volume() +".";
